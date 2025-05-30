d693e8ef7c64d534b419ae47d38e996dffd513d2
Robi <modic.robert@gmail.com>
2025-05-30 13:26:04 +0200

    front end progress

--- API-search-ui/src/app/app.component.ts
+++ API-search-ui/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ResultWrapperComponent} from './components/result-wrapper/result-wrapper.component';
+import {SearchComponent} from './components/search/search.component';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, ResultWrapperComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
